c23c72aca5504ffa960662d49ed40066d83af7c8
Ozan Yıldız <developer@yildizozan.com>
2021-01-06 06:01:30 +0300

    Lookup table is added for mix columns

--- driver/main.cpp
+++ driver/main.cpp
@@ -1,13 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 
 #include <aes.h>
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
 
 int main(int argc, char *argv[]) {
 
     printf("Hello, World!\n");
 
+    {
+        uint8_t flag = 1;
+
+        state_t input = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0xfe};
+        state_t expected = {0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0xbb};
+
+        printf("%s%s Test%s\n", BLUE, "SubstitutionBytes", RESET);
+
+        Debug("State:", input, 16);
+        SubstitutionBytes(&input);
+        Debug("Result:", input, 16);
+
+        for (int i = 0; i < 16; ++i) {
+            if (input[i] != expected[i]) {
+                flag = 0;
+                break;
+            }
+        }
+
+        Debug("Expect:", expected, 16);
+        if (flag) {
+            printf("%s%s equal! %s\n", GREEN, "SubstitutionBytes", RESET);
+        } else {
+            printf("%s%s not equal!%s\n", RED, "SubstitutionBytesWord", RESET);
+        }
+
+        printf("-----------------------------------\n");
+    }
     {
         uint8_t flag = 1;
         // This is major-column order matrix
@@ -21,7 +68,7 @@ int main(int argc, char *argv[]) {
         printf("%s%s Test%s\n", BLUE, "MixColumns", RESET);
 
         Debug("State:", input, 16);
-        MixColumnsNew(&input);
+        MixColumnsLookupTable(&input);
         Debug("Result:", input, 16);
 
         for (int i = 0; i < 16; ++i) {
@@ -36,6 +83,38 @@ int main(int argc, char *argv[]) {
             printf("%s%s equal! %s\n", GREEN, "MixColumnsNew", RESET);
         } else {
             printf("%s%s not equal!%s\n", RED, "MixColumnsNew", RESET);
+        }
+
+        printf("-----------------------------------\n");
+    }
+    {
+        uint8_t flag = 1;
+        // This is major-column order matrix
+        state_t expected = {
+                0x63, 0x2F, 0xAF, 0xA2, 0xEB, 0x93, 0xC7, 0x20, 0x9F, 0x92, 0xAB, 0xCB, 0xA0, 0xC0, 0x30, 0x2B
+        };
+        state_t input = {
+                0xBA, 0x75, 0xF4, 0x7A, 0x84, 0xA4, 0x8D, 0x32, 0xE8, 0x8D, 0x06, 0x0E, 0x1B, 0x40, 0x7D, 0x5D
+        };
+
+        printf("%s%s Test%s\n", BLUE, "MixRColumnsLookupTable", RESET);
+
+        Debug("State:", input, 16);
+        MixRColumnsLookupTable(&input);
+        Debug("Result:", input, 16);
+
+        for (int i = 0; i < 16; ++i) {
+            if (input[i] != expected[i]) {
+                flag = 0;
+                break;
+            }
+        }
+
+        Debug("Expect:", expected, 16);
+        if (flag) {
+            printf("%s%s equal! %s\n", GREEN, "MixRColumnsLookupTable", RESET);
+        } else {
+            printf("%s%s not equal!%s\n", RED, "MixRColumnsLookupTable", RESET);
         }
 
         printf("-----------------------------------\n");
